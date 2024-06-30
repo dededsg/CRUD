6cbeee2f470ceb5fe28abae2456e1676063cffe8
John <89491969+dededsg@users.noreply.github.com>
2024-06-30 17:50:22 -0300

    update packages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
